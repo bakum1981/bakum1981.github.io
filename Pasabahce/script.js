7aabd4d6b64b9ed363f815aebedbfbdec1fcbdb4
bakum1981 <73237368+bakum1981@users.noreply.github.com>
2020-11-08 23:29:47 +0300

    pasabahce

--- Pasabahce/script.js
+++ Pasabahce/script.js
@@ -1,1 +1,1 @@
-﻿$(document).ready(function(){$(".header__brg").click(function(){$(".header__brg").toggleClass("active"),$(".header__menu").toggleClass("active"),$(".body").toggleClass("lock")})})
+﻿$(document).ready(function(){$(".hamburger").click(function(){$(".hamburger").toggleClass("is-active"),$(".header__menu").toggleClass("active"),$(".body").toggleClass("lock")})})
