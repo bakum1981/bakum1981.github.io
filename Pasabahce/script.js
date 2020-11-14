4ecaf6ef005ed594e98288eec9c22e8c58ced41c
bakum1981 <73237368+bakum1981@users.noreply.github.com>
2020-11-15 00:30:42 +0300

    Pasabahce

--- Pasabahce/script.js
+++ Pasabahce/script.js
@@ -4,4 +4,20 @@ $(document).ready(function() {
 		$('.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+
+	const cart = this.querySelector('.main__item-cart');
+	const halfHeight = cart.offsetHeight / 2;	
+	const halfWidth = cart.offsetWidth / 2;
+
+	
+
+	const rotate = (event) =>  {		
+		cart.style.transform = 'rotateX('+-(event.offsetY - halfHeight) / 5 + 'deg) rotateY('
+	+ (event.offsetX - halfWidth) / 5 + 'deg)';	
+	}
+
+
+	cart.addEventListener('mousemove', rotate);
+
+
 });
