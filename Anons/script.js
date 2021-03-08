6f38208b78256c137cd234c5c18eae057dd9e04f
bakum1981 <73237368+bakum1981@users.noreply.github.com>
2021-03-09 01:19:52 +0300

    Anons

--- Anons/script.js
+++ Anons/script.js
@@ -1,2 +1,3 @@
-$(document).ready(function(){const rotate=(event)=>{cart.style.transform='rotateX('+-(event.offsetY-halfHeight)/5+'deg) rotateY('+(event.offsetX-halfWidth)/5+'deg)'}
-cart.addEventListener('mousemove',rotate)})
+$(document).ready(function(){function rotateCart(){const cart=document.querySelector('.anons__content-cart');function rotate(event){const halfHeight=cart.offsetHeight/2;const halfWidth=cart.offsetWidth/2;cart.style.transform='rotateX('+-(event.offsetY-halfHeight)/5+'deg) rotateY('+(event.offsetX-halfWidth)/5+'deg)'}
+if(cart){cart.addEventListener('mousemove',rotate)}}
+rotateCart()})
